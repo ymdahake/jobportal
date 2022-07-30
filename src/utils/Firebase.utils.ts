02e4e404cdf8f3b913a5b907a16cc71e49b0101c
ymdahake <ymdahake@gmail.com>
2022-07-30 14:55:27 +0530

    Change in firebase config

--- src/utils/Firebase.utils.ts
+++ src/utils/Firebase.utils.ts
@@ -13,12 +13,21 @@ import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABThgZ2I22K4-J69-l0rhshyFynXxgTEI",
-  authDomain: "jobportal-8b13d.firebaseapp.com",
-  projectId: "jobportal-8b13d",
-  storageBucket: "jobportal-8b13d.appspot.com",
-  messagingSenderId: "87085176023",
-  appId: "1:87085176023:web:1c4e513184b4b479228c92",
+  //Below is the app registration configuration of Yogesh firebase
+  // apiKey: "AIzaSyABThgZ2I22K4-J69-l0rhshyFynXxgTEI",
+  // authDomain: "jobportal-8b13d.firebaseapp.com",
+  // projectId: "jobportal-8b13d",
+  // storageBucket: "jobportal-8b13d.appspot.com",
+  // messagingSenderId: "87085176023",
+  // appId: "1:87085176023:web:1c4e513184b4b479228c92",
+
+  //Below is the app registraction congfigruation of Amit
+  apiKey: "AIzaSyCpP0HVu_tEbtdGx19BckAliZzVSar9JLE",
+  authDomain: "jobportal-2a30b.firebaseapp.com",
+  projectId: "jobportal-2a30b",
+  storageBucket: "jobportal-2a30b.appspot.com",
+  messagingSenderId: "797108216179",
+  appId: "1:797108216179:web:042dc18b4ce844224ae506"
 };
 
 // Initialize Firebase
